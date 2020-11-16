8b45c02ff2f1cc4af0d269b42e66026c16e2db1e
Vandanavkp <74441452+Vandanavkp@users.noreply.github.com>
2020-11-16 20:43:05 +0530

    Update ILoanService.java

--- ELoanSystem/src/com/iiht/evaluation/eloan/service/ILoanService.java
+++ ELoanSystem/src/com/iiht/evaluation/eloan/service/ILoanService.java
@@ -1,9 +1,11 @@
 package com.iiht.evaluation.eloan.service;
 
+import com.iiht.evaluation.eloan.exception.ELoanSystemException;
 import com.iiht.evaluation.eloan.model.LoanInfo;
+
 
 public interface ILoanService {
 
-	void applyLoan(LoanInfo loan1);
+	LoanInfo createLoan(LoanInfo loan)throws ELoanSystemException;
 
 }
